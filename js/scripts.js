170769fad7d002a76460f73df4bc782276d8d9d7
Matthew Wilkin and Olivia Hinton <olivia@>
2016-06-15 15:21:29 -0700

    add feature to gather player name and display

--- js/scripts.js
+++ js/scripts.js
@@ -37,16 +37,20 @@ $(document).ready(function() {
   $("button#start").click(function(event){
     $(".player-console").show();
     $(".start-menu").hide();
+
+    var player1Name = $(".player1Name").val();
+    $("#player1Name").text(player1Name);
+
+    var player2Name = $(".player2Name").val();
+    $("#player2Name").text(player2Name);
+
   })
 
   $("button#new-game").click(function(event){
     $(".player-console").hide();
     $(".start-menu").show();
+    tempScore = 0;
   })
-
-
-
-
 
   $("button#player1-roll").click(function(event){
     event.preventDefault();
