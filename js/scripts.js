0115fa15304cceab4ec5466084b0bd32b524cd43
Matthew Wilkin and Olivia Hinton <olivia@>
2016-06-15 10:01:45 -0700

    add html elements to set up page

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// //business logic
+// var totalScore = 0;
+//
+// function BankAccount (name, initialdeposit, balance, deposit, withdrawal) {
+//   this.userName = name;
+//   this.initialdeposit = initialdeposit;
+//   this.userBalance = balance;
+//   this.userDeposit = deposit;
+//   this.userWithdrawal = withdrawal;
+// }
