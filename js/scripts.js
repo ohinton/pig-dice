d8ef0f371abd5c2f4028a26e89528000b2f7ce0c
Matt Wilkin and Olivia Hinton <matt@>
2016-06-16 11:14:53 -0700

    update code to include constructors and prototypes

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,68 @@
 //business logic
-
-var tempScore = 0;
-var totalScore1 = 0;
-var totalScore2 = 0;
-var roll = 0;
-
-// function player(name, score) {
-//   this.playerName = name;
-//   this.gameScore = score;
-// }
-
-// PlayerInfo.prototype.roundScore = function () {
-//   roundTotal += this.player1DiceRoll
-// }
-//
-// PlayerInfo.prototype.totalScore = function () {
-//   totalScore += this.playerRoundTotal
-// }
-
+var player1 = new Player(true);
+var player2 = new Player(false);
+// var tempScore = 0;
+// var totalScore1 = 0;
+// var totalScore2 = 0;
+// var roll = 0;
 
 var throwdice = function () {
-  roll = Math.floor(6*Math.random()) + 1;
-  if (roll === 1) {
-    tempScore = 0;
+  return Math.floor(6*Math.random())+1;
+}
+
+function Player(turn) {
+  this.roll = 0;
+  this.tempscore = 0;
+  this.totalscore = 0;
+  this.turn = turn;
+}
+
+// checking for 1
+Player.prototype.rollone = function() {
+  if (this.roll === 1) {
+  this.tempscore = 0;
+  this.changeturn();
+} else {
+  this.tempscore += this.roll;
+}
+}
+
+// hold
+Player.prototype.hold = function () {
+  this.totalscore += this.tempscore;
+  this.tempscore = 0;
+  this.changeturn();
+}
+
+// changing turn
+Player.prototype.changeturn = function () {
+  if (this.turn === true) {
+    this.turn = false;
   } else {
-    tempScore += roll;
+    this.turn = true;
   }
-  return roll;
 }
+// check for 100
+// var runningScore1 = function () {
+//   totalScore1 += tempScore;
+//
+//   if (totalScore1 >= 100) {
+//     alert("You win!")
+//   }
+//   return totalScore1;
+// }
+//
+// var runningScore2 = function () {
+//   totalScore2 += tempScore;
+//
+//   if (totalScore2 >= 100) {
+//     alert("You win!")
+//   }
+//
+//   return totalScore2;
+// }
 
-var runningScore1 = function () {
-  totalScore1 += tempScore;
-
-  if (totalScore1 >= 100) {
-    alert("You win!")
-  }
-
-  return totalScore1;
-
-}
-
-var runningScore2 = function () {
-  totalScore2 += tempScore;
-
-  if (totalScore2 >= 100) {
-    alert("You win!")
-  }
-
-  return totalScore2;
-}
-
+// User Interface
 $(document).ready(function() {
   $("button#start").click(function(event){
     $(".player-console").show();
@@ -60,47 +73,39 @@ $(document).ready(function() {
 
     var player2Name = $(".player2Name").val();
     $("#player2Name").text(player2Name);
-
-  })
+  });
 
   $("button#new-game").click(function(event){
     $(".player-console").hide();
     $(".start-menu").show();
-    tempScore = 0;
-  })
+  });
 
   $("button#player1-roll").click(function(event){
-    event.preventDefault();
+    player1.roll = throwdice();
+    $("#die-roll-1").text(player1.roll);
+    player1.rollone();
+    $("#round-total-1").text(player1.tempscore);
+  });
 
-    var result = throwdice();
-
-    $("#die-roll-1").text(result);
-    $("#round-total-1").text(tempScore);
+  $("button#player2-roll").click(function(event){
+    player2.roll = throwdice();
+    $("#die-roll-2").text(player2.roll);
+    player2.rollone();
+    $("#round-total-2").text(player2.tempscore);
   });
 
   $("button#player1-hold").click(function(event){
-    $("#total-score-1").text(runningScore1);
-    tempScore = 0;
+    player1.hold();
+    $("#total-score-1").text(player1.totalscore);
     $("#round-total-1").empty();
     $("#die-roll-1").empty();
-
-});
-
-  $("button#player2-roll").click(function(event){
-    event.preventDefault();
-
-    var result = throwdice();
-
-    $("#die-roll-2").text(result);
-    $("#round-total-2").text(tempScore);
   });
 
   $("button#player2-hold").click(function(event){
-    $("#total-score-2").text(runningScore2);
-    tempScore = 0;
+    player2.hold();
+    $("#total-score-2").text(player2.totalscore);
     $("#round-total-2").empty();
     $("#die-roll-2").empty();
-
   });
 
 });
